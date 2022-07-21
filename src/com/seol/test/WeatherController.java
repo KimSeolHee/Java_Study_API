2bbbb2fea1af2f7535e91eb48e4079c37bcb6f57
KimSeolHee <tjfgml64@nate.com>
2022-07-22 08:50:07 +0900

    test

---
+++ src/com/seol/test/WeatherController.java
@@ -0,0 +1,18 @@
+package com.seol.test;
+
+public class WeatherController {
+
+	//WeatherController
+	//start메서드 하나
+	//1.번누르면 도시정보 초기화 - init메서드
+	//2. 전국날씨 출력
+	//3. 지역날씨 검색 도시명검색해서 해당도시 날씨찍기 find
+	//4. 지역정보 추가
+	//5. 지역정보 삭제
+	//6. 종료
+	
+	public void start() {
+		
+	}
+
+}
---
+++ src/com/seol/test/WeatherMain.java
@@ -0,0 +1,15 @@
+package com.seol.test;
+
+public class WeatherMain {
+
+	public static void main(String[] args) {
+		//1.클래스의 역할과 메서드의 역할 변수의 역할
+		//WeatherMain		: 프로그램을 실행,Test 
+		//WeatherService	: 데이터 가공, 실제 로직
+		//WeatherContrllor  : 조종하는 역할(관리역할)
+		//WeatherView		: 정보를 출력하기위한 용도(읽기전용)
+		//CityDTO			: 데이터 덩어리, 데이터운반
+		
+	}
+
+}
